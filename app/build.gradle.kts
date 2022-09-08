98087efa7eaa8ad77c754926bf59400575369ba4
noednett <noednett1@gmail.com>
2022-09-08 18:00:25 +0200

    branching to golang

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,7 +22,17 @@ dependencies {
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+
+	//adding dependencies for flyway
+	implementation("org.flywaydb:flyway-core")
+
+	//adding dependencies for postgres and the required drivers to use it with spring boot
+	implementation("org.springframework.boot:spring-boot-starter-jdbc")
+	implementation("org.springframework.boot:spring-boot-starter-web")
+	implementation("org.postgresql:postgresql")
+
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
+
 }
 
 tasks.withType<KotlinCompile> {
